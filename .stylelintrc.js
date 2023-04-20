e6a18b8206d2368ccc5279dca1594fcded60e883
倏昱 <yufan.yyf@antgroup.com>
2023-04-20 17:18:24 +0800

    💚 ci: fix stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
-  plugins: ['stylelint-less'],
+  extends: ['stylelint-config-recommended', 'stylelint-config-prettier'],
+  plugins: ['stylelint-less', 'stylelint-order'],
   customSyntax: 'postcss-less',
-  rules: {},
-  ...require.resolve('@umijs/lint/dist/config/stylelint'),
+  rules: {
+    'selector-id-pattern': null,
+    'selector-class-pattern': null,
+    'number-max-precision': null,
+    'font-family-no-duplicate-names': null,
+    'no-descending-specificity': null,
+    'declaration-block-no-duplicate-properties': null,
+  },
 }
