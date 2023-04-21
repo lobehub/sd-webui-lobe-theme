edaa04359f9f52bb58791a0b46b6017f5e52cd1d
ebw <a13308263216@gmail.com>
2023-04-21 18:40:23 +0800

    🐛 fix: init theme error

---
+++ src/components/theme/useIsDarkMode.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+function checkIsDarkMode() {
+  try {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches;
+  } catch (err) {
+    return false;
+  }
+}
+
+export function useIsDarkMode() {
+  const [isDarkMode, setIsDarkMode] = useState(checkIsDarkMode());
+
+  useEffect(() => {
+    const mqList = window.matchMedia('(prefers-color-scheme: dark)');
+
+    const listener = (event:any) => {
+      setIsDarkMode(event.matches);
+    };
+
+    mqList.addEventListener('change', listener);
+
+    return () => {
+      mqList.removeEventListener('change', listener);
+    };
+    }, []);
+
+  return isDarkMode;
+}
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,45 +1,59 @@
-import favicon from '@/script/favicon'
-import formatPrompt from '@/script/format-prompt'
-import promptBracketChecker from '@/script/prompt-bracket-checker'
-import '@/theme/style.less'
-import { ThemeProvider, setupStyled } from 'antd-style'
-import qs from 'query-string'
-import React, { useEffect, useState } from 'react'
-import { createRoot } from 'react-dom/client'
-import { ThemeContext } from 'styled-components'
-import App from './App'
+import favicon from "@/script/favicon";
+import formatPrompt from "@/script/format-prompt";
+import promptBracketChecker from "@/script/prompt-bracket-checker";
+import "@/theme/style.less";
+import { ThemeProvider, setupStyled } from "antd-style";
+import qs from "query-string";
+import React, { useEffect, useState } from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeContext } from "styled-components";
+import App from "./App";
+import { useIsDarkMode } from "@/components/theme/useIsDarkMode";
 
 const Root: React.FC = () => {
-  setupStyled({ ThemeContext })
-  const [appearance, setAppearance] = useState<'light' | 'dark'>('light')
+  setupStyled({ ThemeContext });
+  const isDarkMode = useIsDarkMode();
+  const [appearance, setAppearance] = useState<"light" | "dark">("light");
+  const [first, setFirst] = useState(true);
   useEffect(() => {
-    const themeMode: any = String(qs.parseUrl(window.location.href).query.__theme) || 'light'
-    setAppearance(themeMode)
-    document.body.classList.add(themeMode)
-  }, [])
-
+      const queryTheme = String(qs.parseUrl(window.location.href).query.__theme || "")
+      if (queryTheme){
+        setAppearance(queryTheme as any);
+        document.body.classList.add(queryTheme);
+        return
+      }
+      setAppearance(isDarkMode ? "dark" : "light");
+      document.body.classList.add(isDarkMode ? "dark" : "light");
+  }, [isDarkMode]);
+  useEffect(()=>{
+    if (first){
+      setFirst(false);
+      return;
+    }
+    window.location.reload();
+  },[isDarkMode])
   return (
     <ThemeProvider appearance={appearance}>
       <App themeMode={appearance} />
     </ThemeProvider>
-  )
-}
+  );
+};
 
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.createElement('div')
-  root.setAttribute('id', 'root')
-  gradioApp().append(root)
-  const client = createRoot(root)
-  client.render(<Root />)
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.createElement("div");
+  root.setAttribute("id", "root");
+  gradioApp().append(root);
+  const client = createRoot(root);
+  client.render(<Root />);
+});
 
 onUiLoaded(() => {
-  favicon()
-})
+  favicon();
+});
 
 onUiUpdate(() => {
-  formatPrompt()
-  promptBracketChecker()
-})
+  formatPrompt();
+  promptBracketChecker();
+});
 
 export default () => null
