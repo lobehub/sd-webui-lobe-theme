096acaf31925fb1e76bf32c29110dac089394ef7
canisminor1990 <i@canisminor.cc>
2023-04-22 12:36:29 +0800

    :lipstick: style: better tool button

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -9,49 +9,6 @@ const SidebarView = styled.div`
   overflow-x: hidden;
   overflow-y: auto;
   height: -webkit-fill-available;
-
-  #quicksettings {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
-    justify-content: stretch;
-
-    > div > div > div > label {
-      max-width: 80%;
-    }
-
-    input[type='color'] {
-      width: 100%;
-    }
-
-    input[type='number'],
-    textarea {
-      height: 20px !important;
-      padding: 4px;
-    }
-
-    > * {
-      flex: 1;
-      max-width: unset !important;
-      min-width: unset !important;
-      width: 100%;
-      margin: 0;
-      padding: 0;
-    }
-
-    span {
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      width: 100%;
-    }
-
-    .dropdown-arrow {
-      min-width: 16px;
-      min-height: 16px;
-    }
-  }
 `
 
 interface SidebarProps {
