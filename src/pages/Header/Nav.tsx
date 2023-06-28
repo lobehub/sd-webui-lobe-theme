163f445e0c27ff559132926f7373e683c5c9d389
canisminor1990 <i@canisminor.cc>
2023-06-28 22:55:26 +0800

    🐛 fix(nav): fix nav active key [#164]

--- src/pages/Header/Nav.tsx
+++ src/pages/Header/Nav.tsx
@@ -1,13 +1,20 @@
 import { Burger, TabsNav, type TabsNavProps } from '@lobehub/ui';
 import { useResponsive } from 'antd-style';
 import { startCase } from 'lodash-es';
-import { memo, useEffect, useState } from 'react';
+import { memo, useCallback, useEffect, useMemo, useState } from 'react';
+import { shallow } from 'zustand/shallow';
+
+import { useAppStore } from '@/store';
 
 const hideOriganlNav = () => {
   (gradioApp().querySelector('#tabs > .tab-nav:first-of-type') as HTMLDivElement).style.display =
     'none';
 };
 
+const getNavTabs = (): HTMLDivElement[] =>
+  Array.prototype.slice.call(
+    gradioApp().querySelectorAll('#tabs > [id^="tab_"]') as NodeListOf<HTMLDivElement>,
+  );
 const getNavButtons = (): HTMLButtonElement[] =>
   Array.prototype.slice.call(
     gradioApp().querySelectorAll(
@@ -15,33 +22,47 @@ const getNavButtons = (): HTMLButtonElement[] =>
     ) as NodeListOf<HTMLButtonElement>,
   );
 
-const onChange: TabsNavProps['onChange'] = (e) => {
+interface NavItem {
+  id: string;
+  index: number;
+  label: string;
+}
+const genNavList = (): NavItem[] => {
+  const navList = getNavTabs();
   const buttons = getNavButtons();
-  buttons[Number(e)]?.click();
+  return buttons.map((button, index) => {
+    const id = navList[index].id;
+    return {
+      id,
+      index,
+      label: startCase(String(button.textContent)),
+    };
+  });
 };
 
 const Nav = memo(() => {
+  const currentTab = useAppStore((st) => st.currentTab, shallow);
   const { mobile } = useResponsive();
   const [opened, setOpened] = useState(false);
   const [items, setItems] = useState<TabsNavProps['items']>([]);
 
+  const navList = useMemo(() => genNavList(), []);
+
+  const onChange: TabsNavProps['onChange'] = useCallback(
+    (id: string) => {
+      const index = navList.find((nav) => nav.id === id)?.index || 0;
+      const buttonList = getNavButtons();
+      buttonList[index].click();
+    },
+    [navList],
+  );
+
   useEffect(() => {
     hideOriganlNav();
-    const buttons = getNavButtons();
-    const list: TabsNavProps['items'] = buttons.map((button: HTMLButtonElement, index: number) => {
-      button.id = `kitchen-nav-${index}`;
+    const list: TabsNavProps['items'] = navList.map((item) => {
       return {
-        key: String(index),
-        label: (
-          <div
-            onClick={() => {
-              const buttons = getNavButtons();
-              buttons[index]?.click();
-            }}
-          >
-            {startCase(String(button.textContent))}
-          </div>
-        ),
+        key: item.id,
+        label: <div onClick={() => onChange(item.id)}>{item.label}</div>,
       };
     });
     setItems(list.filter(Boolean));
@@ -49,7 +70,7 @@ const Nav = memo(() => {
 
   if (mobile) return <Burger items={items} opened={opened} setOpened={setOpened} />;
 
-  return <TabsNav items={items} onChange={onChange} />;
+  return <TabsNav activeKey={currentTab} items={items} onChange={onChange} />;
 });
 
 export default Nav;
--- src/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Theme as AntdStyleTheme } from 'antd-style';
-
-declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends AntdStyleTheme {}
-}
