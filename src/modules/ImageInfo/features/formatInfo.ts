cd50661a14d8b2724ed4d1e8c567926d0d41a410
bluelovers <codelovers@users.sourceforge.net>
2024-05-06 15:41:43 +0800

    style: eslint

--- src/modules/ImageInfo/features/formatInfo.ts
+++ src/modules/ImageInfo/features/formatInfo.ts
@@ -5,49 +5,12 @@ const formatPrompt = (prompt: string) => {
   return Converter.convert(newPrompt);
 };
 
-export const formatInfo = (info: string) => {
-  if (!info || info === 'undefined') return;
-  if (!info.includes('<br>')) return;
-
-  let position: string = '';
-  let negative: string = '';
-  let config: string = '';
-
-  let data = info?.split('Negative prompt:').filter(Boolean);
-  if (data[1]) {
-    position = data[0];
-    data = data[1].split('Steps:');
-    negative = `Negative prompt: ${data[0]}`;
-    config = `Steps: ${data[1]}`;
-  } else {
-    data = data[0].split('Steps:');
-    position = data[0];
-    config = `Steps: ${data[1]}`;
-  }
-
-  position = position.trim().replaceAll('<br>', '\n');
-  negative = negative.trim().replaceAll('<br>', '\n');
-  config = config.trim().replaceAll('<br>', '\n');
-
-  if (!config.includes(',')) return;
-
-  const configs = infoparser(config);
-
-  position = position ? formatPrompt(position) : '';
-  negative = negative ? formatPrompt(negative.split('Negative prompt: ')[1]) : '';
-
-  return {
-    config: configs,
-    negative: negative,
-    positive: position,
-  };
-};
-
-function infoparser(line: string) {
+function infoparser(lineInput: string) {
   let output: Record<string, string> = {};
   let buffer = '';
   let key: string;
   let quotes = false;
+  let line = lineInput;
   if (line.at(-1) === ':') line = line.slice(0, -1);
 
   let c_pre: string;
@@ -88,3 +51,41 @@ function infoparser(line: string) {
   if (key) output[key] = buffer.slice(1);
   return output
 }
+
+export const formatInfo = (info: string) => {
+  if (!info || info === 'undefined') return;
+  if (!info.includes('<br>')) return;
+
+  let position: string = '';
+  let negative: string = '';
+  let config: string = '';
+
+  let data = info?.split('Negative prompt:').filter(Boolean);
+  if (data[1]) {
+    position = data[0];
+    data = data[1].split('Steps:');
+    negative = `Negative prompt: ${data[0]}`;
+    config = `Steps: ${data[1]}`;
+  } else {
+    data = data[0].split('Steps:');
+    position = data[0];
+    config = `Steps: ${data[1]}`;
+  }
+
+  position = position.trim().replaceAll('<br>', '\n');
+  negative = negative.trim().replaceAll('<br>', '\n');
+  config = config.trim().replaceAll('<br>', '\n');
+
+  if (!config.includes(',')) return;
+
+  const configs = infoparser(config);
+
+  position = position ? formatPrompt(position) : '';
+  negative = negative ? formatPrompt(negative.split('Negative prompt: ')[1]) : '';
+
+  return {
+    config: configs,
+    negative: negative,
+    positive: position,
+  };
+};
