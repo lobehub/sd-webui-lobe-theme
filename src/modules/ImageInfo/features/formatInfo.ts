aa1504b12946b8acd789510da2b5c8b3c6477b09
bluelovers <codelovers@users.sourceforge.net>
2024-05-17 10:38:43 +0800

    Update formatInfo.ts (#569)

--- src/modules/ImageInfo/features/formatInfo.ts
+++ src/modules/ImageInfo/features/formatInfo.ts
@@ -1,10 +1,26 @@
 import { parseFromRawInfo } from '@bluelovers/auto1111-pnginfo';
 
-import { Converter } from '@/scripts/formatPrompt';
+import { splitSmartly } from 'split-smartly2';
 
 const formatPrompt = (prompt: string) => {
-  let newPrompt = prompt.replaceAll('&lt;', '<').replaceAll('&gt;', '>');
-  return Converter.convert(newPrompt);
+  let newPrompt = prompt
+    .replaceAll('&lt;', '<')
+    .replaceAll('&gt;', '>')
+    .replace(/^[\x00\s,，。]+$/gm, '')
+    .replace(/\n{2,}/g, '\n')
+  ;
+
+  const entries = splitSmartly(newPrompt.replace(/\n/g, '<br>'), [',','，','。'], {
+    brackets: true,
+    trimSeparators: true,
+  }) as string[];
+
+  return entries
+    .filter(line => line.length)
+    .join(', ')
+    .replace(/<br>/g, '\n')
+    .replace(/^\s+|\s+$/gm, '')
+    ;
 };
 
 export const formatInfo = (info: string) => {
